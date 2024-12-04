cd3623bd81a83c9da64384e80b4c296f2cb9b352
Frixuu <kontakt@lukasz.xyz>
2024-12-04 18:18:55 +0100

    Fix downloaded .vsix being compressed and unextractable

--- src/language_server.rs
+++ src/language_server.rs
@@ -1,7 +1,18 @@
-use std::path::PathBuf;
+use std::{
+    io::{Cursor, Read},
+    path::PathBuf,
+};
 
-use zed::{DownloadedFileType, LanguageServerInstallationStatus::*};
-use zed_extension_api::{self as zed, LanguageServerId, LanguageServerInstallationStatus};
+use flate2::read::GzDecoder;
+use zed_extension_api::{
+    self as zed,
+    http_client::{fetch, HttpMethod::Get, HttpRequest, RedirectPolicy::*},
+    LanguageServerId,
+    LanguageServerInstallationStatus::{self, *},
+};
+use zip::ZipArchive;
+
+use crate::extension;
 
 pub const MARKETPLACE_API_URL: &str = "https://marketplace.visualstudio.com/_apis/public";
 pub const VSHAXE_AUTHOR: &str = "nadako";
@@ -25,11 +36,38 @@ fn set_maybe_status(id: Option<&LanguageServerId>, status: &LanguageServerInstal
 /// Forcefully downloads a specified version of the language server.
 pub fn download_fresh(id: Option<&LanguageServerId>, version: &str) -> Result<PathBuf, String> {
     set_maybe_status(id, &Downloading);
-    zed::download_file(
-        download_url(version).as_ref(),
-        format!("vshaxe-{version}").as_ref(),
-        DownloadedFileType::Zip, //.vsix are zips
-    )?;
+
+    // Download the Visual Studio extension package.
+    // This distribution method was chosen, because the alternatives are less convenient:
+    // - The GitHub releases for the language server contain only the source code,
+    //   which would need to be compiled (and additional packages installed) to run.
+    // - The GitHub packages are precompiled, but the repo does not allow anonymous downloads.
+    let vsix_bytes = fetch(
+        &HttpRequest::builder()
+            .method(Get)
+            .url(download_url(version))
+            .redirect_policy(FollowLimit(5))
+            .header("accept", "application/vsix")
+            .header("accept-encoding", "gzip, deflate")
+            .header("user-agent", "Zed extension for Haxe v0.1")
+            .build()?,
+    )?
+    .body;
+
+    // The .vsix release is gzipped...
+    let mut decoder = GzDecoder::new(Cursor::new(&vsix_bytes));
+    let mut buffer_intermediate = Vec::with_capacity(4_000_000);
+    decoder
+        .read_to_end(&mut buffer_intermediate)
+        .map_err(|e| e.to_string())?;
+
+    // but it actually is a ZIP archive underneath.
+    // Extract it somewhere in our extension's working directory
+    ZipArchive::new(Cursor::new(&buffer_intermediate))
+        .map_err(|e| e.to_string())?
+        .extract(extension::working_dir().join(format!("vshaxe-{version}")))
+        .map_err(|e| e.to_string())?;
+
     set_maybe_status(id, &None);
     Ok(instance_dir_path(version))
 }
