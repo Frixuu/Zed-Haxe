7ec62226a895a7aa4710c6c8ed9fba2dac0385d4
Frixuu <kontakt@lukasz.xyz>
2025-04-18 19:43:13 +0200

    Update HXML grammar

--- languages/hxml/highlights.scm
+++ languages/hxml/highlights.scm
@@ -1,29 +1,6 @@
-[
-	(class_path_id)
-	(connect_id)
-	(dce_id)
-	(define_id)
-	(library_id)
-	(main_id)
-	(output_id)
-	(output_id_no_value)
-	(output_id_run)
-	(remap_id)
-	(resource_id)
-	(server_connect_id)
-	(server_listen_id)
-
-	(no_param)
-] @keyword
-
-(comment) @comment
-
-[
- ":"
- "="
- "@"
-] @keyword.operator
-
-(text) @string
+[(flag) (switch) (next) (each)] @keyword
+[":" "=" "@"] @keyword.operator
+[(text) (identifier) (url) (version)] @string
 (value) @number
 (number) @number
+(comment) @comment
