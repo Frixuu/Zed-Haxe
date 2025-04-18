3493183afaa020e60fd3e422d2755c5a8b92f41f
Frixuu <kontakt@lukasz.xyz>
2025-04-18 12:12:58 +0200

    Add HXML grammar

---
+++ languages/hxml/highlights.scm
@@ -0,0 +1,29 @@
+[
+	(class_path_id)
+	(connect_id)
+	(dce_id)
+	(define_id)
+	(library_id)
+	(main_id)
+	(output_id)
+	(output_id_no_value)
+	(output_id_run)
+	(remap_id)
+	(resource_id)
+	(server_connect_id)
+	(server_listen_id)
+
+	(no_param)
+] @keyword
+
+(comment) @comment
+
+[
+ ":"
+ "="
+ "@"
+] @keyword.operator
+
+(text) @string
+(value) @number
+(number) @number
